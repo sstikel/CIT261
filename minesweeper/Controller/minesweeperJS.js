845c763573d20952fd07b1bbb147bf634367b823
Samuel Gay <sstikel@gmail.com>
2019-07-11 14:04:40 -0600

    Mine number counters are working (also increments mine...)

--- minesweeper/Controller/minesweeperJS.js
+++ minesweeper/Controller/minesweeperJS.js
@@ -1,9 +1,47 @@
+/*********************************************************************************
+ * Program: Minesweeper
+ * Class: CIT 261
+ * Proffesor: Thompson
+ * Author: Sam Gay
+ * Date: Spr 2019
+ * 
+ * Purpose: recreate the minesweeper game to be played in a web browser, 
+ * specifically designed for mobile usage.
+ * 
+ * Game Play: Find the mines (without clicking on one) by clicking on a tile
+ * to reveal what's below. 
+ * 
+ * If a mine is revealed, you lose; 
+ * 
+ * if a number is revealed, it represents how many mines are touching that 
+ * tile (top, bottom, left, right, and all 4 corners); 
+ * 
+ * if the tile is blank, there are no mines touching it;
+ * 
+ * if all tiles are revealed except mines, player wins.
+ *********************************************************************************/
+
+ /**
+  * Notes of code:
+  * - 1000 => mine
+  * - 999 => flag
+  * - 0 => empty
+  * - 1 => one mine touching
+  * - 2-8 => two to eight mines touching
+  * 
+  * Fixes to make:
+  * - 'setNumbers' is incrementing the mine numbers
+  * 
+  */
+
+
+
 ////////////////////////////////INITIAL VALUES///////////////////////////////////
 let fieldSizeFactor = 10;
 let fieldSize = fieldSizeFactor * fieldSizeFactor; 
 let mineCount = fieldSizeFactor;
 let mineLocation = [];
-let allLocations = [fieldSize];
+let allLocations = new Array(fieldSize); 
 
 ////////////////////////////////MINES////////////////////////////////////////////
 /*
@@ -18,20 +56,33 @@ function calcMineCount() {
 * mine location setter
 */
 //store mine locations to array.
+//TODO - find out why sometimes 11 mines are created...
+//may want to use 'forEach' instead of second 'for'
 function setMineLocation(){
    //randomize mine location
    for (var i = 0; i < mineCount; i++){
     let num = Math.floor(Math.random() * fieldSize);
-    for(var j = 0; j < mineLocation.length; j++) {
-      if (mineLocation[j] == num){
-        i--;
-        break;
-      }
+    if(!mineLocation.includes(num)){
+      allLocations[num] = 1000;
+      mineLocation.push(num);
     }
-    mineLocation.push(num);
-    console.log(mineLocation[i]);
-    allLocations[num] = "m";
+    else{
+      i--;
+    }
+    // for(var j = 0; j < mineLocation.length; j++) {
+    //   //if duplicate found, rerun
+    //   if (mineLocation[j] == num){
+    //     i--;
+    //     break;
+    //   }
+    // }
+    // mineLocation.push(num);
+    // //console.log(mineLocation[i]);
+    // allLocations[num] = "m";
+    
   }
+  console.log("mineLocation[]: ");
+  console.log(mineLocation);
   console.log("allLocations[]: ");
   console.log(allLocations);
 }
@@ -40,90 +91,103 @@ function setMineLocation(){
 /**
  * Set numbers
  */
-/*
-function setNumbers(){
-mineLocation.foreach(function(mine){
-  let top = mine.value - fieldSizeFactor;
-  let bottom = mine.value + fieldSizeFactor;
 
-  //above -- Is there a line above?
-  if(!(top < 0)){
-    //left   -- End of row?
-    if ((top - 1) % fieldSizeFactor >= 0){
-      if (allLocations[top-1] == ""){
-        allLocations[top-1] = 1;
-      }
-      else
-        allLocations[top-1]++;
-    }
-    //center -- mine - 10
-    if (allLocations[top] == ""){
-      allLocations[top] = 1;
-    }
-    else
-      allLocations[top]++;
-    //right  -- End of row?
-    if ((top + 1) % fieldSizeFactor <= fieldSizeFactor - 1){
-      if (allLocations[top+1] == ""){
-        allLocations[top+1] = 1;
-      }
-      else
-        allLocations[top+1]++;
+ //set empty tiles to 0
+function setNumbers(){
+  for(var i = 0; i < fieldSize; i++){
+    if ( allLocations[i] != 1000){
+      allLocations[i] = 0;
     }
   }
-  
-  //beside
-    if ((mine.value - 1) % fieldSizeFactor >= 0){
-      if (allLocations[mine.value-1] == ""){
-        allLocations[mine.value-1] = 1;
-      }
-      else
-        allLocations[mine.value-1]++;
+  console.log("allLocations[]: ");
+  console.log(allLocations);
+ 
+  mineLocation.forEach(function(mine){
+    
+    //spot right above mine
+    let aboveMine = mine - fieldSizeFactor;
+    //spot right below mine
+    let belowMine = mine + fieldSizeFactor;
+   
+
+    /*
+    *line above - works
+    */
+   //check for top line
+   if(aboveMine >= 0){
+     if(!(aboveMine >= 1000))
+      allLocations[aboveMine]++;
+
+     ////Left////
+     //check for leftmost position 
+     if(!((aboveMine - 1) % fieldSizeFactor == (fieldSizeFactor - 1))){
+      if(!(aboveMine-1 >= 1000))
+        allLocations[aboveMine-1]++;
+     }
+
+
+     ////Right////
+     //check for rightmost position
+     if(!((aboveMine + 1) % fieldSizeFactor == 0)){
+       if(!(aboveMine+1 >= 1000))
+        allLocations[aboveMine+1]++;
+     }
+   }
+
+    /*
+    *same line - works
+    */
+   ////left////
+   //check for position 0 - no position left of 0
+   if(mine != 0){
+    //check for left most position - no wrapping
+    if((mine - 1) % fieldSizeFactor != (fieldSizeFactor - 1)){
+      if(!(mine-1 >= 1000))
+        allLocations[mine-1]++;
     }
-    //right  -- End of row?
-    if ((mine.value + 1) % fieldSizeFactor <= fieldSizeFactor - 1){
-      if (allLocations[mine.value+1] == ""){
-        allLocations[mine.value+1] = 1;
-      }
-      else
-        allLocations[mine.value+1]++;
+   }
+
+   ////right////
+   //check for position 99 - no position right of 99
+   if(mine <= (fieldSize - 1)){
+    //check for right most position - no wrapping
+    if((mine + 1) % fieldSizeFactor != 0){
+      if(!(mine+1 >= 1000))
+        allLocations[mine+1]++;
     }
-  
-  //below -- Is there a line below?
-  if(!(bottom > fieldSize)){
-    //left   -- End of row?
-    if ((bottom - 1) % fieldSizeFactor >= 0){
-      if (allLocations[bottom-1] == ""){
-        allLocations[bottom-1] = 1;
-      }
-      else
-        allLocations[bottom-1]++;
-    }
-    //center --
-    if (allLocations[bottom] == ""){
-      allLocations[bottom] = 1;
-    }
-    else
-      allLocations[bottom]++;
-    //right  -- End of row?
-    if ((bottom + 1) % fieldSizeFactor <= fieldSizeFactor - 1){
-      if (allLocations[bottom+1] == ""){
-        allLocations[bottom+1] = 1;
-      }
-      else
-        allLocations[bottom+1]++;
-    }
-  }
-});
+   }
+
+    /*
+    *line below - works
+    */
+   //check for bottom line
+   if(belowMine >= (fieldSizeFactor - 1)){
+     if(!(belowMine >= 1000))
+      allLocations[belowMine]++;
+
+     ////Left////
+     //check for leftmost position 
+     if(!((belowMine - 1) % fieldSizeFactor == (fieldSizeFactor - 1))){
+       if(!(belowMine-1 >= 1000))
+        allLocations[belowMine-1]++;
+     }
+
+
+     ////Right////
+     //check for rightmost position
+     if(!((belowMine + 1) % fieldSizeFactor == 0)){
+       if(!(belowMine+1 >= 1000))
+        allLocations[belowMine+1]++;
+     }
+   }    
+  });
 }
-*/
+
 
 /////////////////////////////////Empty Spaces/////////////////////////////////
 function setEmptyTiles(){
   allLocations.forEach(element => {
-    if('m'!=element || '1'!=element || '2'!=element || '3'!=element || '4'!=element || '5'!=element || '6'!=element || '7'!=element || '8'!=element){
-      element = ' '
-    }
+   
   });
 }
 
@@ -164,7 +228,7 @@ function buildMineField(){
  //set mines
  setMineLocation();
  //set numbers
- //setNumbers();  //TODO
+ setNumbers();
  //set empty tiles
  setEmptyTiles();
 
@@ -173,31 +237,31 @@ function buildMineField(){
     let currentLocation = allLocations[i];
     let type = "";
     //check tile content
-    if('m' == currentLocation){
+    if(currentLocation >= 1000){
       type = "tiles_mine";
     }
-    else if('1' == currentLocation){
+    else if(1 == currentLocation){
       type = "tiles_number number--1";
     }
-    else if('2' == currentLocation){
+    else if(2 == currentLocation){
       type = "tiles_number number--2";
     }
-    else if('3' == currentLocation){
+    else if(3 == currentLocation){
       type = "tiles_number number--3";
     }
-    else if('4' == currentLocation){
+    else if(4 == currentLocation){
       type = "tiles_number number--4";
     }
-    else if('5' == currentLocation){
+    else if(5 == currentLocation){
       type = "tiles_number number--5";
     }
-    else if('6' == currentLocation){
+    else if(6 == currentLocation){
       type = "tiles_number number--6";
     }
-    else if('7' == currentLocation){
+    else if(7 == currentLocation){
       type = "tiles_number number--7";
     }
-    else if('8' == currentLocation){
+    else if(8 == currentLocation){
       type = "tiles_number number--8";
     }
     else {
@@ -210,3 +274,10 @@ function buildMineField(){
 }
 
 ////////////////////////////////GAME PLAY///////////////////////////////////
+
+
+////Items to include////
+//empty tile clicked, reveal all connected empty tiles
+//mine clicked - mine shown in red, other mines revealed, dead smiley
+//all tiles clicked EXCEPT mines - game wins, display flags on mines
+ 
